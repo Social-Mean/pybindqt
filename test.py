423958cccb145d9265fbe74f31e09e93a8f7e74a
ming <2052760@tongji.edu.cn>
2025-07-27 10:07:07 +0800

    调整import顺序，防止警告

--- test.py
+++ test.py
@@ -1,12 +1,12 @@
 import os
 import sys
+sys.path.append("./build/")
+from QtWidgets import QApplication, QMainWindow  # , QPushButton
 
 qt_dir = os.environ["QTDIR"]
 os.environ["QT_QPA_PLATFORM_PLUGIN_PATH"] = f"{qt_dir}/plugins/platforms"
 
-sys.path.append("./build/")
 
-from QtWidgets import QApplication, QMainWindow  # , QPushButton
 # from QtCore import Signal
 
 app = QApplication(["app"])
