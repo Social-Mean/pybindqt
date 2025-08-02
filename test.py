a65cbd80d9bf809623273cba0e9961f01e923de1
ming <2052760@tongji.edu.cn>
2025-08-02 10:04:31 +0800

    refactor: delete unused imports

--- test.py
+++ test.py
@@ -1,7 +1,7 @@
 import os
 import sys
 sys.path.append("./build/")
-from QtWidgets import QApplication, QMainWindow, QPushButton, QObject
+from QtWidgets import QApplication, QMainWindow, QPushButton
 
 qt_dir = os.environ["QTDIR"]
 os.environ["QT_QPA_PLATFORM_PLUGIN_PATH"] = f"{qt_dir}/plugins/platforms"
