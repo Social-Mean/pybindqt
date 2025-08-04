5168b0183f57279d98a539ccf305bb939c979c0e
ming <2052760@tongji.edu.cn>
2025-08-04 19:24:19 +0800

    修复缺少头文件无法编译的问题

--- bind_qmainwindow.cpp
+++ bind_qmainwindow.cpp
@@ -1,7 +1,17 @@
 #include <pybind11/pybind11.h>
 
+#include <QDockWidget>
 #include <QMainWindow>
+#include <QMenu>
+#include <QMenuBar>
+#include <QStatusBar>
+#include <QString>
+#include <QToolBar>
 #include <QWidget>
+#include <Qt>
+
+#include "qstring_converter.h"
+
 namespace py = pybind11;
 
 void bind_qmainwindow(py::module_ &m) {
