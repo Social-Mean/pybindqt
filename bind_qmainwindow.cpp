390911e2d0db263cc19d84d2e5264db933b1bc20
ming <2052760@tongji.edu.cn>
2025-08-04 19:17:57 +0800

    清理 QMainWIndow 里的函数

--- bind_qmainwindow.cpp
+++ bind_qmainwindow.cpp
@@ -7,11 +7,5 @@ namespace py = pybind11;
 void bind_qmainwindow(py::module_ &m) {
   py::class_<QMainWindow, QWidget>(m, "QMainWindow")
       .def(py::init([]() { return std::make_unique<QMainWindow>(); }))
-      .def("show", &QMainWindow::show)
-      .def("setWindowTitle",
-           [](QMainWindow *self, const std::string &title) {
-             self->setWindowTitle(QString::fromStdString(title));
-           })
-      .def("resize",
-           [](QMainWindow *self, int w, int h) { self->resize(w, h); });
+      .def("show", &QMainWindow::show);
 }
